91c915432ebd459ceaef6b6a285188761f797093
Jiayue Mao <jiayuemao@Jiayues-MacBook-Pro.local>
2019-09-09 15:28:23 -0400

    modify files: calculator/static/css/style.css; calculator/view.py; calculator/templates/calculator/calculator.html

--- homework/2/calculator/views.py
+++ homework/2/calculator/views.py
@@ -15,39 +15,46 @@ def calculator(request):
     return render(request, 'calculator/calculator.html', context)
 
 def init():
-    context = {"display" : 0, "cur_val" : 0, "pre_val" : 0, "pre_opt" : "+", "error" : 0}
+    context = {"display" : 0, "cur_val" : 0, "pre_val" : 0, "pre_opt" : "+", "cur_opt" : "", "error" : 0}
     return context
 
 def error():
-    context = {"display" : "E", "cur_val" : 0, "pre_val" : 0, "pre_opt" : "+", "error" : 1}
+    context = {"display" : "ERROR", "cur_val" : 0, "pre_val" : 0, "pre_opt" : "+", "cur_opt" : "", "error" : 1}
     return context
 
 def updateValue(request, val):
     cur_val = int(request.POST["cur_val"])
     display = cur_val * 10 + int(request.POST["num"])
     pre_opt = request.POST["pre_opt"]
-    context = {"display" : display, "cur_val" : display, "pre_val" : request.POST["pre_val"], "pre_opt" : pre_opt, "error" : 0}
+    context = {"display" : display, "cur_val" : display, "pre_val" : request.POST["pre_val"],
+    "pre_opt" : pre_opt, "cur_opt" : "", "error" : 0}
     return context
 
 def updateOpt(request, opt):
     pre_opt = request.POST["pre_opt"]
     pre_val = int(request.POST["pre_val"])
     cur_val = int(request.POST["cur_val"])
-    if pre_opt == "+":
-        display = pre_val + cur_val
-    if pre_opt == "-":
-        display = pre_val - cur_val
-    if pre_opt == "*":
-        display = pre_val * cur_val
-    if pre_opt == "/":
-        if request.POST["opt"] == "=" and cur_val == 0:
-            context = error()
-            return context
-        display = int(pre_val / cur_val);
+    cur_opt = request.POST["cur_opt"]
+    if cur_opt == "=":
+        display = request.POST["display"]
+    else:
+        if cur_opt == "*" or cur_opt == "/":
+            cur_val = 1
+        if pre_opt == "+":
+            display = pre_val + cur_val
+        if pre_opt == "-":
+            display = pre_val - cur_val
+        if pre_opt == "*":
+            display = pre_val * cur_val
+        if pre_opt == "/":
+            if cur_val == 0:
+                return error()
+            display = int(pre_val / cur_val)
+
     if request.POST["opt"] == "=":
-        context = {"display" : display, "cur_val" : 0, "pre_val" : 0, "pre_opt" : "+", "error" : 0}
+        context = {"display" : display, "cur_val" : 0, "pre_val" : 0, "pre_opt" : "+", "cur_opt" : "=", "error" : 0}
         return context
     else:
         pre_opt = request.POST["opt"]
-        context = {"display": display, "cur_val": 0, "pre_val" : display, "pre_opt" : pre_opt, "error" : 0}
+        context = {"display": display, "cur_val": 0, "pre_val" : display, "pre_opt" : pre_opt, "cur_opt" : pre_opt, "error" : 0}
         return context
