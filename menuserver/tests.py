8cf1f4b93f47f3b00da03ee99f4c80a45ddeb35c
Jiayue Mao <jiayuemao@Jiayues-MacBook-Pro.local>
2019-11-10 02:49:33 -0500

    modify tests.py; modify requirement.txt to include webdriver-manager

--- menuserver/tests.py
+++ menuserver/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, LiveServerTestCase, Client, override_settings
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import Select
 import time
 from selenium.webdriver.common.keys import Keys
@@ -11,7 +12,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 # Create your tests here.
 class UserTestCase(LiveServerTestCase):
     def setUp(self):
-        self.selenium = webdriver.Chrome()
+        self.selenium = webdriver.Chrome(ChromeDriverManager().install())
 
     def tearDown(self):
         self.selenium.quit()
@@ -70,7 +71,7 @@ class UserTestCase(LiveServerTestCase):
 class MenuManagementTestCase(LiveServerTestCase):
 
     def setUp(self):
-        self.selenium = webdriver.Chrome()
+        self.selenium = webdriver.Chrome(ChromeDriverManager().install())
 
     def tearDown(self):
         self.selenium.quit()
@@ -151,7 +152,7 @@ class DishTestCase(LiveServerTestCase):
 class OrderTestCase(LiveServerTestCase):
 
     def setUp(self):
-        self.selenium = webdriver.Chrome()
+        self.selenium = webdriver.Chrome(ChromeDriverManager().install())
 
     def tearDown(self):
         self.selenium.quit()
@@ -197,7 +198,7 @@ class OrderModelTestCase(LiveServerTestCase):
 class SubmittedOrderTestCase(LiveServerTestCase):
 
     def setUp(self):
-        self.selenium = webdriver.Chrome()
+        self.selenium = webdriver.Chrome(ChromeDriverManager().install())
 
     def tearDown(self):
         self.selenium.quit()
@@ -287,7 +288,7 @@ class SubmittedOrderModelTestCase(LiveServerTestCase):
 class StoreTestCase(LiveServerTestCase):
 
     def setUp(self):
-        self.selenium = webdriver.Chrome()
+        self.selenium = webdriver.Chrome(ChromeDriverManager().install())
 
     def tearDown(self):
         self.selenium.quit()
@@ -377,7 +378,7 @@ class RoleTestCase(LiveServerTestCase):
 class ManagerTestCase(LiveServerTestCase):
 
     def setUp(self):
-        self.selenium = webdriver.Chrome()
+        self.selenium = webdriver.Chrome(ChromeDriverManager().install())
 
     def tearDown(self):
         self.selenium.quit()
