6804eee0b7e4137ad32e1f09548c0e6f68749db4
Jiayue Mao <jiayuemao@Jiayues-MacBook-Pro.local>
2019-11-10 01:55:11 -0500

    change requirement.txt to include selenium

--- menuserver/views.py
+++ menuserver/views.py
@@ -18,7 +18,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import Group
 
-import simplejson as json
+import json
 
 # Create your views here.
 def main(request):
